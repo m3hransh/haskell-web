7b7ef74a8a8810335b1ccb84a3ecca1091e0a24d
Zahra Khodabakhshian <zahrakhodabakhshian54@gmail.com>
2024-05-24 19:06:54 +0200

    Add Header and Routing

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,7 +5,23 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+
+          "darkgray" : "#0E0D11",
+          "100": '#44475a',
+          '200': '#6272a4',
+          '300': '#bd93f9',
+          '400': '#ff79c6',
+          '500': '#8be9fd',
+          '600': '#50fa7b',
+          '700': '#f1fa8c',
+          '800': '#ffb86c',
+          '900': '#ff5555',
+  
+      },
+      
+    },
   },
   plugins: [],
 }
