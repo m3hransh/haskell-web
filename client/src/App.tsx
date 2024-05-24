a505ede5a30de226894d28dfd966ffc6b6e47e3b
Zahra Khodabakhshian <zahrakhodabakhshian54@gmail.com>
2024-05-24 19:43:29 +0200

    Revert "Add Header and Routing"
    This reverts commit 7b7ef74a8a8810335b1ccb84a3ecca1091e0a24d.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,18 +1,8 @@
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
-import AboutMe from "./components/AboutMe";
-import Header from "./components/Header";
+import Comment from "./components/Comments";
 const App = () => {
  
   return (
-    <>
-   <Router>
-      <Header />
-      <Routes>
-        <Route path="/about" element={<AboutMe name="zagrs" description="" questionText="" socialLink="" socialLinkText="" mainImageUrl="" />} />
-      </Routes>
-    </Router>
-
-    </>
+    <Comment />
   );
 }
 
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,23 +5,7 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      colors: {
-
-          "darkgray" : "#0E0D11",
-          "100": '#44475a',
-          '200': '#6272a4',
-          '300': '#bd93f9',
-          '400': '#ff79c6',
-          '500': '#8be9fd',
-          '600': '#50fa7b',
-          '700': '#f1fa8c',
-          '800': '#ffb86c',
-          '900': '#ff5555',
-  
-      },
-      
-    },
+    extend: {},
   },
   plugins: [],
 }
